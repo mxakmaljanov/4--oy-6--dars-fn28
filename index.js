f59a81842933cbf20ffc97151a37d6838138cb62
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-10-11 08:20:21 +0500

    6--dars

---
+++ index.js
@@ -0,0 +1,121 @@
+//1-masala
+//function sum(x, y) {
+//    return x + y;
+//}
+//console.log(sum(3, 2));
+//2-masla
+//function t(m) {
+//    return m * 60;
+//}
+//console.log(t(5));
+////3-masala
+//function n(i) {
+//    return i + 1;
+//}
+//
+//console.log(n(0));
+//4-masla
+//function uy(asos, balandlik) {
+//    return (asos * balandlik) / 2;
+//}
+//console.log(uy(3, 2));
+////5-masala
+//function a(age) {
+//    return age * 365;
+//}
+//console.log(a(65)); 
+////6-masala
+//function kub(x) {
+//    return x * x * x;
+//}
+//console.log(kub(3));
+////7-masala
+//function e (arr) {
+//    return arr[0];
+//}
+//console.log(e([1, 2, 3]));
+//8-masala
+//function p(x, y) {
+//    return x * y;
+//}
+//console.log(p(230, 10));
+//9-masala
+//function s(hour) {
+//    return hour * 3600;
+//}
+//console.log(s(2));
+//10-misol
+//function ut(tomon1, tomon2) {
+//    return (tomon1 + tomon2) - 1;
+//}
+//console.log(ut(8, 10));
+//11-masala
+//function q(son1, son2) {
+//    return son1 % son2;
+//}
+//console.log(q(1, 3));
+//12-masla
+//function ty(boyi, eni) {
+//    return boyi * eni;
+//}
+//
+//console.log(turtburchakYuzi(6, 7));
+//13-masala
+//function stringQoshish(a) {
+//    return " AKMALJANOV " + a;
+//}
+//console.log(stringQoshish("MAXMUDXON")); 
+//14-masala
+//function kvadrat(x) {
+//    return x * x;
+//}
+//console.log(kvadrat(5));
+//15-masla
+//function n(raqam) {
+//    return raqam <= 0;
+//}
+//console.log(n(5)); 
+//16-masala
+//function ib(n) {
+//    return (n - 2) * 180;
+//}
+//console.log(ib(3));
+//17-masla
+//function o(x, y) {
+//    return (x * 2) + (y * 3);
+//}
+//console.log(o(1, 1));
+//18-masala
+//function n(name) {
+//    return name + "sinfdosh";
+//}
+//console.log(n("Mubasher"));
+//9-masla
+//unction k(x, y) {
+//   return (x + y) < 100;
+//
+//onsole.log(k(22, 15)); 
+//0-masala
+//unction a(n) {
+//   let arr = [];
+//   for(let i = 1; i <= n; i++) {
+//       arr.push(i);
+//   }
+//   return arr;
+//
+//onsole.log(a(1));
+///21-masala
+//unction o(tovuq, qoy, sigir) {
+//   return (tovuq * 2) + (qoy * 4) + (sigir * 4);
+//
+//onsole.log(o(2, 3, 5)); 
+///22-masala
+//unction k(a, b) {
+//   return a && b;
+//
+//
+//et h1 = true;
+//et h2 = false;
+//
+//onsole.log(k(h1, h2)); // ➞ false
+//
